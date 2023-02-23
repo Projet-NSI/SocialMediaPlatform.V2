1ec8fd7e73af24011c71946f5986d6500055541a
Tiborz08 <111081518+Tiborz08@users.noreply.github.com>
2023-02-23 14:18:11 +0100

    correction bugs

--- landing/views.py
+++ landing/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.views import View
+from django.conf import settings
+from django.http import FileResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from pathlib import Path
 
 
 class index(View):
