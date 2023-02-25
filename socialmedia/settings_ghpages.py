204547fe0fedf9bd0475ec01f4f10f0ec5319207
Tiborz08 <111081518+Tiborz08@users.noreply.github.com>
2023-02-25 10:52:03 +0100

    Suppression de ghpages

--- socialmedia/settings_ghpages.py
+++
@@ -1,4 +0,0 @@
-from .settings import *
-
-ALLOWED_HOSTS = ['https://projet-nsi.github.io/SocialMediaPlatform.V2/']
-HOME_PAGE = BASE_DIR / 'templates/landing/index.html'
--- socialmedia/wsgi.py
+++ socialmedia/wsgi.py
@@ -11,17 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# Déterminez si nous sommes sur la branche gh-pages
-on_ghpages_branch = 'gh-pages' in os.environ.get('GITHUB_REF', '')
 
-# Si nous sommes sur la branche gh-pages, utilisez les paramètres spécifiques à gh-pages
-if on_ghpages_branch:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialmedia.settings_ghpages')
-else:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialmedia.settings')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialmedia.settings')
-
-application = get_wsgi_application()
 
 
